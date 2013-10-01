e7fc95aaf65a0508be722671d62eab40907ba07e
Swaagie <info@martijnswaagman.nl>
2013-10-01 12:31:22 +0200

    [major] add debouncer and add temper files

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ var Notify = require('fs.notify');
 exports.name = 'watch';
 
 /**
+ * Debounce function to defer the call of the supplied `fn` with `wait` ms. The
+ * timer will be reset as long as the function is called.
+ *
+ * @param {Function} fn function to call
+ * @param {Number} wait milliseconds
+ * @api private
+ */
+function debounce(fn, wait) {
+  return function defer() {
+    var context = this
+      , args = arguments
+      , timestamp = Date.now()
+      , result, timeout;
+
+    function later() {
+      var last = Date.now() - timestamp;
+      if (last < wait) return timeout = setTimeout(later, wait - last);
+
+      result = fn.apply(context, args);
+    }
+
+    if (!timeout) timeout = setTimeout(later, wait);
+    return result;
+  };
+}
+
+/**
  * Server side plugin to watch pages, pagelets and views for easy reloading
  * during development.
  *
@@ -19,25 +46,40 @@ exports.name = 'watch';
  * @api public
  */
 exports.server = function server(bigpipe, options) {
-  var files = Object.keys(bigpipe.compiler.alias)
-    , notifications = new Notify(files);
+  var files = Object.keys(bigpipe.temper.file)
+    , notifications;
 
-  notifications.on('change', function change(file) {
-    files.forEach(function loopFiles(path) {
+  //
+  // Also add compiler cache items.
+  //
+  Object.keys(bigpipe.compiler.alias).forEach(function loopCompilerCache(item) {
+    if (!~files.indexOf(item)) files.push(item);
+  });
+
+  /**
+   * Check cache and prefetch if the file is part of the compiler.
+   *
+   * @param {String} file name
+   * @api private
+   */
+  function change(file) {
+    files.forEach(function loopCompiler(path) {
       if (~path.indexOf(file)) {
-        //
-        // Delete the content from cache and prefetch again.
-        //
         delete bigpipe.temper.file[path];
         delete bigpipe.temper.compiled[path];
 
         bigpipe.temper.prefetch(path);
       }
+
+      // TODO add compiler stuff.
     });
 
-    //
-    // Notify the developer of changes and reloaded files.
-    //
-    console.log('File changes detected, refreshing paths containing: ' + file);
-  });
+    console.log('File changes detected, refreshing compiler cache using: ' + file);
+  }
+
+  //
+  // Notify the developer of changes and reload files.
+  //
+  notifications = new Notify(files);
+  notifications.on('change', debounce(change, 100));
 };
